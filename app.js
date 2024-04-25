bb99c1a3d57a0469bcee1e8a642558aef6460617
Ellie-Aghajani <z.aqajani@gmail.com>
2024-04-24 18:31:37 -0700

    route recepies

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to Munchimaster!');
 })
 
-
+app.get('/recepies', (req, res) => {
+    res.send(['soup', 'pizza', 'chicken']);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is listening on port ${port}`));
