59d48c4f9a5ff0a6c00ce9590ed2d85178f302be
Ellie-Aghajani <z.aqajani@gmail.com>
2024-04-25 17:43:06 -0700

    add public folder

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const Joi = require('joi');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 const recepies = [
     {id: 1, name:'pizza'},
     {id: 2, name:'rice'},
@@ -19,16 +22,12 @@ app.get('/api/recepies', (req, res) => {
 });
 
 app.post('/api/recepies', (req, res) => {
-    const schema = Joi.object({
-        name:Joi.string().min(3).required()
-    });
-    const {error, value} = schema.validate(req.body);
+
+    const {error} = validateRecepie(req.body);
 
 
-    if(error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
+   
     const recepie = {
         id: recepies.length + 1,
         name:req.body.name
@@ -41,7 +40,7 @@ app.post('/api/recepies', (req, res) => {
 
 app.get('/api/recepies/:id', (req, res)=>{
     const recepie = recepies.find(r => r.id === parseInt(req.params.id));
-    if(!recepie) res.status(404).send('The recepie with the given id was not found');
+    if(!recepie) return res.status(404).send('The recepie with the given id was not found');
     res.send(recepie);
 });
 
@@ -49,23 +48,43 @@ app.put('/api/recepies/:id', (req, res)=>{
     // Look up the r
     const recepie = recepies.find((r)=>r.id === parseInt(req.params.id));
     // If not existing, return 404
-    if(!recepie) res.status(404).send('recepie not found');
+    if(!recepie) return res.status(404).send('recepie not found');
     res.send(recepie);
     // Validate
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
-    const {error, value} = schema.validate(req.body);
+   
+    const {error} = validateRecepie(req.body);
     // If invalid, return 400 - Bad request
-    if(error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
+    
     // Update r
     recepie.name = req.body.name;
     // Return the updated r
     res.send(recepie);
+});
+
+app.delete('/api/recepies/:id', (req, res)=>{
+    // Look up the r
+    const recepie = recepies.find(r => r.id === parseInt(req.params.id));
+
+    // Not existing, return 404
+    if (!recepie)return res.status(404).send('recepie not found');
+   
+    // Delete
+    const index = recepies.indexOf(recepie);
+    recepies.splice(index, 1);
+    // Return the same r
+    res.send(recepie);
 })
+
+
+function validateRecepie(recepie){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(recepie);
+};
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is listening on port ${port}`));
