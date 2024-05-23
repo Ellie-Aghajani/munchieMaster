abe0e25e582aae50613a6e35aff1fc072b341fdb
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-23 16:56:15 -0700

    clean up user constant

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const userSchema = new mongoose.Schema({
+
+const User = mongoose.model('User', new mongoose.Schema({
     name:{
         type: String,
         required: true,
@@ -10,30 +11,30 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        unique: true
+        unique: true,
+        minLength:5,
+        maxLength:255
     },
     password: {
         type: String,
         required: true,
-        minLength:8,
-        maxLength:10
+        minLength:5,
+        maxLength:1024
     }
-});
-
-const User = mongoose.model('User', userSchema);
+}));
 
 function validateUser(user) {
     const schema = Joi.object({
-        name: Joi.string().min(5).required(),
-        email: Joi.string().required(),
-        password: Joi.string().min(8).max(10).required()
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255)..required(),
+        password: Joi.string().min(5).max(255).required()
 
-    })
+    });
     return schema.validate(user);
 }
 
 
-module.exports.userSchema = userSchema;
+
 module.exports.User = User;
 module.exports.validate = validateUser;
 
