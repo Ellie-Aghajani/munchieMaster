ad9fdd90048cb015f2a70424111398ea499e3adc
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-23 17:38:24 -0700

    corrected the name  and email order

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,9 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email})
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User(_.pick(req.body, ['name', 'password', 'email']));
+    user = new User(_.pick(req.body, ['name','email' ,'password' ]));
     await user.save();
-    res.send( _.pick(user, ["_id","name"," email"]));
+    res.send( _.pick(user, ['_id','email',"name"]));
 });
 
 
