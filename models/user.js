1ba68d8d032965e277c6a3885bfc52e5a0be3293
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-23 17:47:29 -0700

    add password complexity to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
+
+
+const complexityOptions = {
+    min: 8,
+    max: 26,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 1,
+    symbol: 1,
+    requirementCount: 4,
+};
+
 
 
 const User = mongoose.model('User', new mongoose.Schema({
@@ -27,7 +40,7 @@ function validateUser(user) {
     const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required(),
-        password: Joi.string().min(5).max(255).required()
+        password: passwordComplexity(complexityOptions).required()
 
     });
     return schema.validate(user);
