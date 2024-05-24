3446b0da6789abfdbff06481e7362c25c6d64ee8
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-23 17:34:55 -0700

    use lodash _.pick

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose'); //node_8.mongo data validation_7.mp4
 const express = require('express');
@@ -9,16 +10,12 @@ router.post('/', async (req, res) => {
     const {error} = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({emai: req.body.email})
+    let user = await User.findOne({email: req.body.email})
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User ({ //const not let because mongodb driver sets the objectId here and no need to reset the variable later
-        name:req.body.name,
-        email: req.body.email,
-        password: req.body.password
-     });
-    await user.save()
-    res.send(user);
+    user = new User(_.pick(req.body, ['name', 'password', 'email']));
+    await user.save();
+    res.send( _.pick(user, ["_id","name"," email"]));
 });
 
 
