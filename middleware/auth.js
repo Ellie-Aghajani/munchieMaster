c627fc87bfe79ac0befe2c19e7cc026d7905f9db
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-24 19:52:14 -0700

    export auth module

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 
 
-function auth(req, res, next){
+module.exports = function(req, res, next){
 
     const token = req.header('x-auth-token');
     if(!token) return res.status(401).send('Access denied. No token provided.')
@@ -23,5 +23,5 @@ function auth(req, res, next){
 };
 
 
-module.exports = auth;
+
 
