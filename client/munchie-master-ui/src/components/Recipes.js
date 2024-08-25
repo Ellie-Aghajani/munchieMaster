3a9bf7c8999172b4f7d00b0da619887a65c71f10
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-24 19:46:47 -0700

    display images and details

--- client/munchie-master-ui/src/components/Recipes.js
+++ client/munchie-master-ui/src/components/Recipes.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, List, ListItem, ListItemText, Paper } from '@mui/material';
+import { Container, Typography, List, ListItem, ListItemText, Paper, ListItemAvatar, Avatar, Divider } from '@mui/material';
 import axios from 'axios';
 
 function Recipes() {
@@ -31,16 +31,29 @@ function Recipes() {
       <List>
         {recipes.map((recipe) => (
           <ListItem key={recipe.id} component={Paper} elevation={2} sx={{ mb: 2, p: 2 }}>
+            {recipe.image && (
+              <ListItemAvatar>
+                <Avatar variant="square" src={`http://localhost:3001${recipe.image}`} alt={recipe.name} sx={{ width: 100, height: 100 }} />
+              </ListItemAvatar>
+            )}
             <ListItemText
               primary={<Typography variant="h6">{recipe.name}</Typography>}
 
               secondary={
-                <Typography variant="body2" color="text.secondary">
-                  Ingredients: {recipe.ingredients.join(', ')},
-                  
-                </Typography> 
+                <>
+                  <Typography variant="body2" color="text.secondary">
+                    Preparation Time: {recipe.preparationTime}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Ingredients: {recipe.ingredients.join(', ')}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Directions: {recipe.directions.join(', ')}
+                  </Typography>
+                </>
               }
             />
+            <Divider sx={{ my: 1 }} />
           </ListItem>
         ))}
       </List>
