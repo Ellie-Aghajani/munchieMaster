ae177d173f8aa473291208ad6e45e193f6f39f04
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-24 22:40:56 -0700

    activate prettier code format

--- client/munchie-master-ui/src/App.js
+++ client/munchie-master-ui/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link as RouterLink } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
-import Login from './components/Login';
-import Recipes from './components/Recipes';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link as RouterLink,
+} from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
+import Login from "./components/Login";
+import Recipes from "./components/Recipes";
 
 function App() {
   return (
@@ -13,16 +18,29 @@ function App() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               MunchieMaster
             </Typography>
-            <Button color="inherit" component={RouterLink} to="/">Home</Button>
-            <Button color="inherit" component={RouterLink} to="/login">Login</Button>
-            <Button color="inherit" component={RouterLink} to="/recipes">Recipes</Button>
+            <Button color="inherit" component={RouterLink} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={RouterLink} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={RouterLink} to="/recipes">
+              Recipes
+            </Button>
           </Toolbar>
         </AppBar>
         <Container>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/recipes" element={<Recipes />} />
-            <Route path="/" element={<Typography variant="h2" component="h1" gutterBottom>Welcome to MunchieMaster</Typography>} />
+            <Route
+              path="/"
+              element={
+                <Typography variant="h2" component="h1" gutterBottom>
+                  Welcome to MunchieMaster
+                </Typography>
+              }
+            />
           </Routes>
         </Container>
       </div>
