37583e1c23921382800a064aaec38b1195285909
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-25 22:57:03 -0700

    auth updated

--- server/app.js
+++ server/app.js
@@ -1,29 +1,28 @@
-
-const winston = require('winston');
-const express = require('express');
-const cors = require('cors');
+const winston = require("winston");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
 
 //Serve static files from 'public/uploads'
-app.use('/uploads', express.static('public/uploads'));
+app.use("/uploads", express.static("public/uploads"));
 
-
-require('./startup/logging')();
-require('./startup/routes')(app);
-require('./startup/db')();//we call the function
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/prod')(app);
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")(); //we call the function
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3001;
-const server = app.listen(port, () => winston.info(`App is listening on port ${port}`));
-
+const server = app.listen(port, () =>
+  winston.info(`App is listening on port ${port}`)
+);
 
 module.exports = server;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,51 +1,47 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const passwordComplexity = require('joi-password-complexity');
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const _ = require('lodash');
-const {User} = require('../models/user');
-const mongoose = require('mongoose'); //node_8.mongo data validation_7.mp4
-const express = require('express');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const passwordComplexity = require("joi-password-complexity");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User } = require("../models/user");
+const mongoose = require("mongoose"); //node_8.mongo data validation_7.mp4
+const express = require("express");
 const router = express.Router();
 
-const complexityOptions = {
-    min: 8,
-    max: 26,
-    lowerCase: 1,
-    upperCase: 1,
-    numeric: 1,
-    symbol: 1,
-    requirementCount: 4,
-};
+// const complexityOptions = {
+//     min: 8,
+//     max: 26,
+//     lowerCase: 1,
+//     upperCase: 1,
+//     numeric: 1,
+//     symbol: 1,
+//     requirementCount: 4,
+// };
 
+router.post("/", async (req, res) => {
+  // const {error} = validate(req.body);
+  // if(error) return res.status(400).send(error.details[0].message);
 
-router.post('/', async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
 
-    const {error} = validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+  const validPassword = bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-    let user = await User.findOne({email: req.body.email})
-    if (!user) return res.status(400).send('Invalid email or password.');
+  const token = user.generateAuthToken();
 
-    const validPassword = bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Invalid email or password.');
-
-    const token = user.generateAuthToken();
-
-    res.send(token);
+  res.send({ token: token });
 });
 
-function validate(req) {
-    const schema = Joi.object({
-        
-        email: Joi.string().min(5).max(255).required(),
-        password: passwordComplexity(complexityOptions).required()
+// function validate(req) {
+//     const schema = Joi.object({
 
-    });
-    return schema.validate(req);
-}
+//         email: Joi.string().min(5).max(255).required(),
+//         password: passwordComplexity(complexityOptions).required()
 
-
+//     });
+//     return schema.validate(req);
+// }
 
 module.exports = router;
