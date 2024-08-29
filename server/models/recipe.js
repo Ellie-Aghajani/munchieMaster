1e20eb6b81138df0189bba67f72fb1dad8766fc4
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-28 18:08:16 -0700

    add like, like count and save

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,51 +1,53 @@
-const mongoose = require('mongoose'); //node_8.mongo data validation_7.mp4
-const Joi = require('joi');
-const {mealSchema} = require('./meal');
+const mongoose = require("mongoose"); //node_8.mongo data validation_7.mp4
+const Joi = require("joi");
+const { mealSchema } = require("./meal");
 
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  preparationTime: {
+    type: String,
+    required: false,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  directions: {
+    type: [String],
+    required: true,
+  },
+  // meal: {
+  //     type:mealSchema,
+  //     required: true
+  // },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  likeCount: { type: Number, default: 0 },
+  savedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
+function validateRecipe(recipe) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    ingredients: Joi.array().items(Joi.string()).min(1).required(),
+    directions: Joi.array().items(Joi.string()).min(1).required(),
+    preparationTime: Joi.string().required(),
+    image: Joi.string().uri(),
+    likeCount: Joi.number().min(0),
+    savedBy: Joi.array().items(Joi.objectId()),
+  });
 
-const Recipe = mongoose.model('Recipe', new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        minLength:5,
-        maxLength:50,
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    preparationTime: {
-        type: String, 
-        required: false
-    },
-    ingredients: {
-        type: [String], 
-        required: true
-    },
-    directions: {
-        type: [String],
-        required: true
-    },
-    meal: {
-        type:mealSchema,
-        required: true
-    }
-}));
-
-
-function validateRecipe(recipe){
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
-        image: Joi.string().uri().optional(),
-        preparationTime: Joi.string().optional(),
-        ingredients: Joi.array().items(Joi.string()).required(),
-        directions: Joi.array().items(Joi.string()).required(),
-        mealId: Joi.objectId().required()
-    });
-    return schema.validate(recipe);
-};
-
+  return schema.validate(recipe);
+}
 
 module.exports.Recipe = Recipe;
 module.exports.validate = validateRecipe;
