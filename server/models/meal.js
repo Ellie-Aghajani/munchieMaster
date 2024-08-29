2402c6462d7ea341114d52541363fe37b42324e3
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-28 18:04:03 -0700

    update meal

--- server/models/meal.js
+++ server/models/meal.js
@@ -1,26 +1,49 @@
-const mongoose = require('mongoose'); //node_8.mongo data validation_7.mp4
-const Joi = require('joi');
+const auth = require("../middleware/auth");
+const mongoose = require("mongoose"); //node_8.mongo data validation_7.mp4
+const express = require("express");
+const router = express.Router();
+const { Meal, validate } = require("../models/meal");
 
-const mealSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        minLength:5,
-        maxLength:50
-    }
+//(route, middleware, actual route handler)
+
+router.get("/", async (req, res) => {
+  const meals = await Meal.find().sort("name");
+  res.send(meals);
 });
 
-const  Meal = mongoose.model('Meal', mealSchema);
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
+  const meal = new Meal({ name: req.body.name });
+  await meal.save();
+  res.send(meal);
+});
 
-function validateMeal(meal){
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    return schema.validate(meal);
-};
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
+  const meal = await Meal.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  );
+  if (!meal) return res.status(404).send("meal not found");
+  res.send(meal);
+});
 
-module.exports.Meal = Meal;
-module.exports.mealSchema = mealSchema;
-module.exports.validate = validateMeal;
+router.delete("/:id", async (req, res) => {
+  const meal = await Meal.findOneAndDelete(req.params.id);
+  if (!meal) return res.status(404).send("meal not found");
+  res.send(meal);
+});
+
+router.get("/:id", async (req, res) => {
+  const meal = await Meal.findById(req.params.id);
+  if (!meal)
+    return res.status(404).send("The meal with the given id was not found");
+  res.send(meal);
+});
+
+module.exports = router;
