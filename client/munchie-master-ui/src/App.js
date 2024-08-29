21b51287c5938a5f292783c544ebae7ad9e7cec5
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-28 17:24:59 -0700

    update user profile

--- client/munchie-master-ui/src/App.js
+++ client/munchie-master-ui/src/App.js
@@ -6,32 +6,36 @@ import {
   Navigate,
   Link as RouterLink,
 } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
 import NavMenu from "./components/NavMenu";
 import Login from "./components/Login";
 import Recipes from "./components/Recipes";
 import AdminRecipeCreator from "./components/AdminRecipeCreator";
 import { AuthProvider } from "./contexts/AuthContext";
 import AdminRoute from "./components/AdminRoute";
+import { ErrorProvider } from "./contexts/ErrorContext";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <NavMenu />
-        <div className="App">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route
-              path="/admin/create-recipe"
-              element={<AdminRoute component={AdminRecipeCreator} />}
-            />
-            <Route path="/" element={<Navigate replace to="/login" />} />
-          </Routes>
-        </div>
-      </Router>
-    </AuthProvider>
+    <ErrorProvider>
+      <AuthProvider>
+        <Router>
+          <NavMenu />
+          <div className="App">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/recipes" element={<Recipes />} />
+              <Route
+                path="/admin/create-recipe"
+                element={<AdminRoute component={AdminRecipeCreator} />}
+              />
+              <Route path="/profile" element={<UserProfile />} />
+              <Route path="/" element={<Navigate replace to="/login" />} />
+            </Routes>
+          </div>
+        </Router>
+      </AuthProvider>
+    </ErrorProvider>
   );
 }
 
