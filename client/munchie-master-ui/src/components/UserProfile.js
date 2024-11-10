ef41db9dc8a6d7b1bedb058b672ac1a5a551ca43
Ellie-Aghajani <z.aqajani@gmail.com>
2024-11-10 03:11:07 -0800

    style

--- client/munchie-master-ui/src/components/UserProfile.js
+++ client/munchie-master-ui/src/components/UserProfile.js
@@ -152,7 +152,13 @@ function UserProfile() {
       >
         <Box>
           <Avatar
-            sx={{ width: 100, height: 100, mr: 2 }}
+            sx={{
+              width: 100,
+              height: 100,
+              mr: 2,
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",
+            }}
             src={
               user?.avatar
                 ? `${config.serverUrl}/uploads/${user.avatar.replace(
